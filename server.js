b6d216a73ed6f0c8731d6775d2183497134cbfad
pgzbc001 <41930066+pgzbc001@users.noreply.github.com>
2021-08-27 22:07:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(8099, () => {
-  console.log('App listening at port 8099')
+app.listen(8080, () => {
+  console.log('App listening at port 8080')
 })
